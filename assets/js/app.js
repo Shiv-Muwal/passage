e7afe14caa730ebab0992a26901f7915933e5858
Shiv-468 <shivmuwal.147@gmail.com>
2024-04-04 11:23:32 +0530

    navbar responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,25 @@
 $('.slider1').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    arrows:false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  });
+  dots: true,
+  infinite: true,
+  speed: 300,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  customPaging: function (slider, i) {
+    return '<a href="#"><img src="./assets/images/Arrow1.svg" /><img src="./assets/images/Arrow2.svg" /></a>';
+  }
+});
+
+const menuicon = document.querySelector(".menuicon");
+const menulist = document.querySelector(".menulist");
+const navLink = document.querySelectorAll(".nav_link");
+navLink.forEach(a => a.addEventListener("click", () => {
+  menulist.classList.toggle("show");
+  document.body.classList.remove("overflow-hidden");
+  menuicon.classList.toggle("show");
+}))
+menuicon.addEventListener("click", () => {
+  menulist.classList.toggle("show");
+  document.body.classList.toggle("overflow-hidden");
+  menuicon.classList.toggle("show");
+});
