60353b3d4e084a65aab95afb46294177bbf7d3c3
Shiv-468 <shivmuwal.147@gmail.com>
2024-04-05 14:02:02 +0530

    remove z-index problem

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,24 +2,29 @@ $('.slider1').slick({
   dots: true,
   infinite: true,
   speed: 300,
+  autoplay:true,
   arrows: false,
   slidesToShow: 1,
   slidesToScroll: 1,
   customPaging: function (slider, i) {
-    return '<a href="#"><img src="./assets/images/Arrow1.svg"/><img src="./assets/images/Arrow2.svg" /></a>';
+    return '<div><img src="./assets/images/svg/Arrow1.svg"/><img src="./assets/images/svg/Arrow2.svg" /></div>';
   }
 });
 
 const menuicon = document.querySelector(".menuicon");
 const menulist = document.querySelector(".menulist");
 const navLink = document.querySelectorAll(".nav_link");
+let index_remove = document.querySelectorAll(".index_remove");
 navLink.forEach(a => a.addEventListener("click", () => {
   menulist.classList.toggle("show");
   document.body.classList.remove("overflow-hidden");
   menuicon.classList.toggle("show");
-}))
+}));
 menuicon.addEventListener("click", () => {
   menulist.classList.toggle("show");
+
   document.body.classList.toggle("overflow-hidden");
   menuicon.classList.toggle("show");
+  index_remove.classList.toggle.z-n1;
+
 });
